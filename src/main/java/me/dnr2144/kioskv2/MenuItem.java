2f6b35c9a3b1fe23b1bca1231d01201fa2b60e9f
seonguk_kang <dnr2144@gmail.com>
2024-11-27 16:12:38 +0900

    Change MenuItem class -> record

--- src/main/java/me/dnr2144/kioskv2/MenuItem.java
+++ src/main/java/me/dnr2144/kioskv2/MenuItem.java
@@ -1,15 +1,6 @@
 package me.dnr2144.kioskv2;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class MenuItem {
-    private final String name;
-    private final double price;
-    private final String menuInfo;
-
+public record MenuItem(String name, double price, String menuInfo) {
     @Override
     public String toString() {
         return String.format("%-15s | W %-4.1f | %s", name, price, menuInfo);
