5d52aa73a00bee5fd87e4783a4f087a0fdd48ae2
seonguk_kang <dnr2144@gmail.com>
2024-11-27 12:25:12 +0900

    Add kiosk v3

---
+++ src/main/java/me/dnr2144/kisoskv3/Constants.java
@@ -0,0 +1,9 @@
+package me.dnr2144.kisoskv3;
+
+public class Constants {
+
+    public static final int EXIT_OPTION = 0;
+    public static final String INVALID_INPUT_MESSAGE = "잘못된 입력입니다. 다시 선택하세요.\n-------------------------------";
+
+    private Constants() {}
+}
---
+++ src/main/java/me/dnr2144/kisoskv3/KioskV3.java
@@ -0,0 +1,48 @@
+package me.dnr2144.kisoskv3;
+
+import me.dnr2144.kioskv2.MenuItem;
+
+import java.util.List;
+import java.util.Scanner;
+
+import static me.dnr2144.kisoskv3.Constants.EXIT_OPTION;
+import static me.dnr2144.kisoskv3.Constants.INVALID_INPUT_MESSAGE;
+
+public class KioskV3 {
+
+    private List<MenuItem> menuItems;
+
+    public KioskV3(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        int input = -1;
+        while (input != EXIT_OPTION) {
+            displayMenu();
+            if (!scanner.hasNextInt()) {
+                System.out.println(INVALID_INPUT_MESSAGE);
+                scanner.nextLine();
+                continue;
+            }
+
+            input = scanner.nextInt();
+            if (input > menuItems.size() || input < 0) {
+                System.out.println(INVALID_INPUT_MESSAGE);
+                continue;
+            }
+
+            String message = input == EXIT_OPTION ? "프로그램을 종료합니다." : menuItems.get(input - 1).toString();
+            System.out.println(message);
+        }
+    }
+
+    public void displayMenu() {
+        System.out.println("[ SHAKESHACK MENU ]");
+        System.out.println("0. 종료");
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.printf("%d. %s\n", (i + 1), menuItems.get(i));
+        }
+    }
+    }
